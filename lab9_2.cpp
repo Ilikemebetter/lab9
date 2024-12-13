55ea538ae5c728da946a5dfcfc51bf2087e433c1
Ilikemebetter <pleum.41733@gmail.com>
2024-12-13 20:00:33 +0700

    lap9_2 completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,24 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//Write the function printO() here
+// Write the function printO() here
+void printO(int i, int j)
+{
+	int  x, y; // i as rows j as columns
+	if (i <= 0 || j <= 0)
+	{
+		cout << "Invalid input";
 
-int main(){
-	
-	printO(2,2);
+	}
+	else
+	{
+		for (x = 0; x < i; x++)
+		{
+			for (y = 0; y < j; y++)
+			{
+				cout << "O";
+			}cout << "\n";
+		}
+		
+	}
+}
+
+int main()
+{
+
+	printO(2, 2);
 	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
+
+	printO(3, 5);
+	cout << "\n";
+
+	printO(5, 3);
+	cout << "\n";
+
+	printO(0, 3);
+	cout << "\n";
+
+	printO(7, -1);
+	cout << "\n";
+
 	return 0;
 }
